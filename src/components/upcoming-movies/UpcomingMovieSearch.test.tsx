dc5d768461c26e15e2a0fa53639a47264e5137d9
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-15 10:30:05 -0600

    test next page button

--- src/components/upcoming-movies/UpcomingMovieSearch.test.tsx
+++ src/components/upcoming-movies/UpcomingMovieSearch.test.tsx
@@ -2,13 +2,28 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import UpcomingMovieSearch from './UpcomingMovieSearch';
 import { MemoryRouter } from 'react-router-dom';
-
-// Fix can't read propery location of undefined
+import userEvent from '@testing-library/user-event';
 
 describe('Upcoming Movie Search Component', () => {
   test('renders to page', () => {
     render(<UpcomingMovieSearch />, { wrapper: MemoryRouter });
 
     expect(screen.getByText(/movies/i)).toBeInTheDocument();
+  });
+
+  test('next page button', () => {
+    const { debug, getByText } = render(<UpcomingMovieSearch />, {
+      wrapper: MemoryRouter,
+    });
+    const nextPage = screen.getByText(/next page/i);
+    const pageText = screen.getByText(/current page: 1/i);
+
+    expect(pageText).toBeInTheDocument();
+    expect(nextPage).toBeInTheDocument();
+
+    userEvent.click(nextPage);
+    expect(pageText).toHaveTextContent(/current page: 2/i);
+
+    debug();
   });
 });
