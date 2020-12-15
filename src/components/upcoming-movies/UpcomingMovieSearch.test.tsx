3e8894b6313f37e8c4984abd2641b9e232676fc5
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-15 10:32:28 -0600

    test both next and prev buttons

--- src/components/upcoming-movies/UpcomingMovieSearch.test.tsx
+++ src/components/upcoming-movies/UpcomingMovieSearch.test.tsx
@@ -11,19 +11,21 @@ describe('Upcoming Movie Search Component', () => {
     expect(screen.getByText(/movies/i)).toBeInTheDocument();
   });
 
-  test('next page button', () => {
+  test('page buttons', () => {
     const { debug, getByText } = render(<UpcomingMovieSearch />, {
       wrapper: MemoryRouter,
     });
     const nextPage = screen.getByText(/next page/i);
+    const prevPage = screen.getByText(/previous page/i);
     const pageText = screen.getByText(/current page: 1/i);
 
     expect(pageText).toBeInTheDocument();
     expect(nextPage).toBeInTheDocument();
+    expect(prevPage).toBeInTheDocument();
 
     userEvent.click(nextPage);
     expect(pageText).toHaveTextContent(/current page: 2/i);
-
-    debug();
+    userEvent.click(prevPage);
+    expect(pageText).toHaveTextContent(/current page: 1/i);
   });
 });
