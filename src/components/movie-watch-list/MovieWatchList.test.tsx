f8965859555196db5ec872edaca238abcc55b84c
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-06 11:31:10 -0600

    test button click in movie watch list creates a new list item

--- src/components/movie-watch-list/MovieWatchList.test.tsx
+++ src/components/movie-watch-list/MovieWatchList.test.tsx
@@ -11,4 +11,12 @@ describe('Movie Watch List', () => {
     const heading = screen.getByRole(/heading/i);
     expect(heading).toBeInTheDocument();
   });
+  test('clicking the add button adds an item to the page', () => {
+    render(<MovieWatchList />, { wrapper: MemoryRouter });
+    const addBtn = screen.getByRole(/button/i, { name: 'Add' });
+    expect(addBtn).toBeInTheDocument();
+    expect(screen.queryByRole(/listitem/i)).not.toBeInTheDocument();
+    userEvent.click(addBtn);
+    expect(screen.queryByRole(/listitem/i)).toBeInTheDocument();
+  });
 });
