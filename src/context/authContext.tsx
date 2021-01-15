442f181d6ac5c49c07d6c26e55cf0dac106b286e
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-15 16:46:30 -0600

    fill out authContext with functions to set user

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,11 +1,36 @@
-import React, { createContext, FC, ReactNode, useContext } from 'react';
+import React, {
+  createContext,
+  FC,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import { auth } from '../firebase';
 
 const initialContext = {};
 
 const AuthContext = createContext(initialContext);
 
 export const AuthContextProvider: FC<ReactNode> = ({ children }) => {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const [currentUser, setCurrentUser] = useState<null | any>(null);
+
+  const signup = (email: string, password: string) => {
+    return auth.createUserWithEmailAndPassword(email, password);
+  };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => setCurrentUser(user));
+    return () => unsubscribe();
+  }, []);
+
+  const shareThisData = { currentUser, signup };
+
+  return (
+    <AuthContext.Provider value={shareThisData}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuthContext = () => {
