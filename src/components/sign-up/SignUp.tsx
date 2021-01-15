dd501a4ccfa3621b7d85d3e4c8cafe03bfe89d20
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-15 14:16:36 -0600

    add signup compoennt bp

---
+++ src/components/sign-up/SignUp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const SignUp = () => {
+  return (
+    <SContainer>
+      <h1>Yo sign yo self up</h1>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.main``;
